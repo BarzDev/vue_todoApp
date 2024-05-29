53fe16f7b13923e02f298b565aa0421a3d098d04
Fakhrul <barz.develop@gmail.com>
2024-05-29 23:20:13 +0700

    fix: postion button

--- src/components/ui/navbar.vue
+++ src/components/ui/navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav
-    class="bg-slate-900 text-white p-3 flex items-center justify-between gap-5 sticky top-0 z-10"
+    class="bg-slate-900 text-white p-3 flex flex-row-reverse items-center justify-between gap-5 sticky top-0 z-10"
   >
     <p class="w-11/12 text-center font-bold">Barz Crud Todo App</p>
     <div class="flex w-1/12 items-center justify-center">
